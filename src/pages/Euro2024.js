9110e36cad94042349405d644dcca343ead6f811
lukakhuza <lukakhuza7@gmail.com>
2024-05-29 11:15:45 -0400

    changing the color of page headings

--- src/pages/Euro2024.js
+++ src/pages/Euro2024.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Euro2024() {
   return (
     <>
-      <h1>Euro 2024</h1>
+      <h1 className="pageHeading">Euro 2024</h1>
       <p>
         Welcome to the Euro 2024 Page for the supporters of Georgian National
         Football Team.
--- src/pages/WorldCup2026.js
+++ src/pages/WorldCup2026.js
@@ -1,7 +1,7 @@
 export default function WorldCup2026() {
   return (
     <>
-      <h1>World Cup 2026</h1>
+      <h1 className="pageHeading">World Cup 2026</h1>
     </>
   );
 }
