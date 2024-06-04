a79ef52f69d42fb0ff947d75d2323e300f6d5b11
lukakhuza <lukakhuza7@gmail.com>
2024-06-04 05:40:52 -0400

    updating links

--- src/pages/WorldCup2026.js
+++ src/pages/WorldCup2026.js
@@ -3,6 +3,9 @@ export default function WorldCup2026() {
     <div className="App-header">
       <h1 className="pageHeading">World Cup 2026</h1>
       <p>United States, Canada, Mexico</p>
+      <p>
+        Go to the <Link to="/euro2024">Euro 2024 page.</Link>
+      </p>
     </div>
   );
 }
