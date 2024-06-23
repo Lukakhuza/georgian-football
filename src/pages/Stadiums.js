9923396be8d89e859bfbcaf48e962167b1fe39c1
Lukakhuza <78911198+Lukakhuza@users.noreply.github.com>
2024-06-22 23:43:46 -0400

    fetching code refactored and error page added. The error  page needs to be enhanced and formatted for when data fails to fetch.

--- src/pages/Stadiums.js
+++ src/pages/Stadiums.js
@@ -1,34 +1,63 @@
 import { useState, useEffect } from "react";
+import Error from "../components/Error";
 
 export default function Stadiums() {
+  const [isFetching, setIsFetching] = useState(false);
   const [stadiums, setStadiums] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    fetch("http://localhost:3001/football/stadiums")
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((resData) => {
+    async function fetchStadiums() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3001/football/stadiums");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch the list of stadiums.");
+        }
         setStadiums(resData.stadiums);
-      });
+      } catch (error) {
+        setError(error);
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchStadiums();
+    // fetch()
+    //   .then((response) => {
+    //     console.log(response);
+    //     return response.json();
+    //   })
+    //   .then((resData) => {
+    //     setStadiums(resData.stadiums);
+    //   });
   }, []);
 
+  console.log(stadiums.length);
+
+  if (error) {
+    return <Error />;
+  }
   return (
     <section className="stadiums-category">
       <h1>Stadiums in Georgia</h1>
-      <ul className="stadiums">
-        {stadiums.map((stadium) => (
-          <li key={stadium.id} className="stadium-item">
-            {/* <div>{stadium.city}</div> */}
-            <img
-              src={`http://localhost:3001/${stadium.image.src}`}
-              alt={stadium.image.alt}
-            />
-            <h4>{stadium.name}</h4>
-          </li>
-        ))}
-      </ul>
+      {stadiums.length === 0 && <p>No Stadiums Available</p>}
+
+      {stadiums.length > 0 && (
+        <ul className="stadiums">
+          {stadiums.map((stadium) => (
+            <li key={stadium.id} className="stadium-item">
+              <img
+                src={`http://localhost:3001/${stadium.image.src}`}
+                alt={stadium.image.alt}
+              />
+              <h4>{stadium.name}</h4>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
