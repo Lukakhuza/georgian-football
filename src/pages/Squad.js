6dd5398a258a70d923f9418d0fab924b0be2b6ac
Lukakhuza <78911198+Lukakhuza@users.noreply.github.com>
2024-07-03 17:19:41 -0400

    styling updated

--- src/pages/Squad.js
+++ src/pages/Squad.js
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+
 export default function Squad() {
-  return <p>The Squad of the Georgian National Team</p>;
+  const [isFetching, setIsFetching] = useState(false);
+  const [squad, setSquad] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlayers() {
+      setIsFetching(true);
+      try {
+        const response = await fetch("http://localhost:3001/football/squad");
+        const resData = await response.json();
+        setSquad(resData.squad);
+      } catch (error) {
+        setError(error);
+      }
+      setIsFetching(false);
+    }
+    fetchPlayers();
+  }, []);
+
+  if (error) {
+    return (
+      <>
+        <p>There was an error</p>
+        <p>{error.message}</p>
+      </>
+    );
+  }
+  console.log(squad);
+
+  return (
+    <section>
+      {squad.length === 0 && squad.length}
+      {squad.length > 0 && (
+        <ul>
+          {squad.map((player) => (
+            <li key={player.id}>
+              {player["first-name"] + " " + player["last-name"]}
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
