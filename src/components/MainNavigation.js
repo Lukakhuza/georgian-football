5fd8d0ad0bbe40e39c4c4c9fa7461192458f50b3
Lukakhuza <78911198+Lukakhuza@users.noreply.github.com>
2024-07-03 13:47:34 -0400

    players data file updated

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -8,9 +8,11 @@ function MainNavigation() {
         <ul className={classes.list}>
           <Link to="/">Home</Link>
 
+          <Link to="/squad">Squad</Link>
+
           <Link to="euro2024">Euro 2024</Link>
 
-          <Link to="worldcup2026">World Cup 2026</Link>
+          {/* <Link to="worldcup2026">World Cup 2026</Link> */}
 
           <Link to="stadiums">Stadiums</Link>
         </ul>
